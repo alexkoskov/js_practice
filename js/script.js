7bcbf6a8fe4152dbcece99324eb7d306a6d790a4
Aleksandr <sunny413x@gmail.com>
2024-08-09 18:24:10 -0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,53 @@
+"use strict";
+
+// const vehicleBodyWidth = 5000;
+// const vehicleBodyHeight = 4000;
+// console.log('width: ' + vehicleBodyWidth + ', height: ' + vehicleBodyHeight);
+
+// const obj = {
+//     name: 'Egor',
+//     age: 11,
+//     isMarried: false 
+// };
+
+// console.log(obj.name);
+
+// alert(obj.name);
+
+// const result = confirm('Are you here?');
+// alert(result);
+
+// const answer = +prompt('Are you above 18?', '');
+// alert(typeof(answer) + answer);
+
+// const answers = [];
+// answers[0] = prompt('Your name', '');
+// answers[1] = prompt('Your surname', '');
+// answers[2] = prompt('Your age', '');
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}`);
+
+// const user = 'Egorka';
+// alert(`Hi, ${user}`);
+
+// console.log(4 + +'5');
+
+// let incr = 10,
+//     decr = 10;
+
+// // incr++;
+// // decr--;
+// // console.log(++incr);
+// // console.log(decr--);
+
+// console.log(5%2);
+
+// console.log(incr==decr);
+
+const isChecked = false,
+    isClose = true;
+
+console.log(!isChecked && isClose);
